b5022b0c7dbeb11833727c079428cca70b7c14a5
frank-kenfack <frankkenfack09@gmail.com>
2026-01-21 13:01:05 +0100

    coorection des erreurs sur tests.py

--- fleet/views.py
+++ fleet/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required  # Pour le login
+from rest_framework.decorators import api_view             # Pour l'API
+from rest_framework.response import Response
+from .models import Vehicule
+from .serializers import VehiculeSerializer
 
-# Cette fonction sert juste à afficher ta page HTML
-def map_view(request):
-    return render(request, 'fleet/map.html')
-
+# 1. La Vue pour la CARTE 
 @login_required # permet la redirection vers Login quand on n'est pas connecté
 def map_view(request):
     return render(request, 'fleet/map.html')
+
+# 2. La Vue pour l'API
+@api_view(['GET'])
+def api_vehicules(request):
+    vehicules = Vehicule.objects.all()
+    serializer = VehiculeSerializer(vehicules, many=True)
+    # On renvoie les données au format GeoJSON
+    return Response({"type": "FeatureCollection", "features": serializer.data})
