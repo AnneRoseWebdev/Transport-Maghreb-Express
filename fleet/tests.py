94befb1d5b545970ff37198e18df577026c53187
abidrakhis6-cloud <abidrakhis6@gmail.com>
2026-01-21 12:15:59 +0100

    Fichier tests.py

--- fleet/tests.py
+++ fleet/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Vehicule
 
-# Create your tests here.
+class VehiculeTests(TestCase):
+    def setUp(self):
+        # On crée un camion virtuel pour le test
+        self.camion = Vehicule.objects.create(
+            immatriculation="TEST-001",
+            statut="GARAGE",
+            carburant_niveau=50.0
+        )
+
+    def test_creation_vehicule(self):
+        """Vérifie que le camion est bien créé en base"""
+        self.assertEqual(self.camion.immatriculation, "TEST-001")
+        print("✅ Test Création OK")
+
+    def test_api_access(self):
+        """Vérifie que l'API répond bien (Code 200)"""
+        response = self.client.get('/api/vehicules/')
+        self.assertEqual(response.status_code, 200)
+        print("✅ Test API OK")
